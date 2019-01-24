77bebc00c4b45acf00b1b82e196794b6e0a73ea5
baitianxin111 <495017135@qq.com>
2019-01-24 18:05:50 +0800

    头和搜索框

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactDOM from 'react-dom';
 import fetchJsonp from 'fetch-jsonp';
 import '../src/style.css';
 import NavBar from './components/NavBar/NavBar'
+import SearchInput from './components/SearchInput/SearchInput'
 
 
 
@@ -10,9 +11,27 @@ import NavBar from './components/NavBar/NavBar'
 class App extends Component {
     constructor (props){
        super(props);
-       this.state = {page: 'book', dataOffset: 0, resetInput: true, 
-        detailShow: false, detailInfo: null, listItems: []};
-        this.getData  = this.getData.bind(this);
+       this.state = {
+         page: 'book', 
+         dataOffset: 0, 
+         resetInput: true, 
+         detailShow: false, 
+         detailInfo: null, 
+         listItems: []
+    };
+       this.getData  = this.getData.bind(this);
+    }
+    //切换底部导航
+    changePage (value){
+        console.log(value)
+       this.setState({
+            page:value,
+            dataOffset :0,
+            resetInput:true,
+            listItems: []
+       },function(){
+           this.getData(this.state.page,'',this.state.dataOffset)
+       })
     }
     //先获取数据
     getData (type, searchInput, start, isUnion, callback) {
@@ -29,7 +48,34 @@ class App extends Component {
                 
             }).then(function (data) {
                 console.log(data);
-            }).catch(function (eror) {
+                let listItems;
+                if(isUnion){
+                    this.state.listItems.map(function (e) {
+                        let len = this.state.listItems.length;
+                        while (len--) {
+                            if (data[params[type][3]][len].id == a.id) {
+                                data[params[type][3]].splice(len, 1);
+                                break;
+                            }
+                            listItems = that.state.listItems.concat(data[params[type][3]]).filter(function(item) {
+                                return !!item.id;
+                            });
+                        }
+                    })
+
+                }else{
+                    listItems = data[params[type][3]].filter(function (item) {
+                        return !!item.id;
+                    })
+                    
+                }
+                //属性重新绑定
+                self.setState({
+                  listItems:listItems      
+                },function(){
+                    callback && callback()
+                })
+            }).catch(function (error) {
                 console.log(error)
             })
 
@@ -40,10 +86,11 @@ class App extends Component {
     render  (){
         return (
          <div className="app">
-         车的股份
-         <NavBar/>
+         随便写写的主页面
+         <NavBar page = {this.state.page} changePage = {this.state.changePage}/>
+         <SearchInput/>
          </div>
         )
     } 
 }
-export default App;
+module.exports =  App;
--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,14 +1,40 @@
-import React,{Component} from 'react';
-import '../../../src/style.css';
+import React, { Component } from 'react';
+import '../../style.css';
 
-class NavBar extends Component{
-  constructor (props){
-      super (props)
-  }
-  render () {
-      return (
-          <div>ceshi 头</div>
-      )
-  }
+class NavBar extends Component {
+
+	togglePage(e) {
+		console.log(e)
+		var page = event.currentTarget.getAttribute('data-page');  
+		let target = e.target || e.srcElement || e.currentTarget;
+		if (!target.dateSet.page) return;
+		console.log(target.dateSet)
+		this.props.changePage(target.dateSet.page || page);//承接父组件传过来的函数
+	}
+
+	constructor(props) {//构造函数
+	    super(props);
+	    this.togglePage = this.togglePage.bind(this);
+	}
+    
+    render() {
+		const page = this.props.page;
+		// console.log(data-page)
+        return (
+	        <div className="nav-bar" onClick={this.togglePage}>
+                <div className="nav-item"  data-page="book" data-active={page=='book'}>
+                	<span>图书</span>
+                </div>
+                <div className="nav-item" data-page="film" data-active={page=='film'}>
+                	<span>电影</span>
+                </div>
+                <div className="nav-item" data-page="music" data-active={page=='music'}>
+                	<span>音乐</span>
+                </div>
+	        </div>
+        );
+    }
 }
-export default NavBar;
+{/* <NavBar page={this.state.page} changePage={this.changePage}/> */}
+// 父组件传递一个值，一个函数，子组件通过this.props.xxxx来获取，页面上data-page 的使用
+module.exports = NavBar;//底部导航栏的组件设计
